36ef1347b61a373aed414cc9ce820af331a8465a
Virtual Machine Administrator <administrator@ubuntu-ror-dev-env.(none)>
2011-12-08 02:26:53 +0000

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,18 @@ class UsersController < ApplicationController
   def new
     @title = "Sign up"
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
 
 
